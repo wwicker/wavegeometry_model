7b15e6f37f8f529513ca163b3767d82b1c73782f
Wolfgang Wicker <wolfgang.wicker@env.ethz.ch>
2021-05-18 17:36:34 +0200

    Bug fixes in basic_state.py
    - there are problems with dask.array.apply_along_axis
    - rewritten to work for numpy.array data instead

--- src/basic_state.py
+++ src/basic_state.py
@@ -1,5 +1,5 @@
 # Python routines to prepare the basic state of a zonally averaged atmosphere
-# based on the wavegeometry model by Nili Harnik
+# based on the wavegeometry model by Nili Harnik and collegues
 # May 2021, Wolfgang Wicker <wolfgang.wicker@env.ethz.ch>
 
 import numpy as np
@@ -8,15 +8,16 @@ from scipy.ndimage import convolve1d
 from scipy.interpolate import interp1d
 
 ## Define a grid class, methods and functions that use numpy.arrays for coordinates and dask.arrays for data
+# EDIT: there is some problem with dask.array.apply_along_axis -> rewritten to use numpy.arrays for everything
 # assume dimensions (time,z,y)
 
-class grid:
+class Grid:
     def __init__(self,namelist,**kwargs):
         # define default parameters
         self.param = dict(ny=47,nz=71,nt=1,
                           top=15,bottom=0,wide=90,hem=1,
                           Ho=7000,om=7.27*10**(-5),ae=6.37*10**6,
-                          Ro=287,g=9.81)
+                          Ro=287,g=9.81,kappa=0.2854)
         
         # read pairs of key and value from namelist
         # ignore lines that start with '#', use whitespaces as delimited
@@ -37,12 +38,12 @@ class grid:
         self.dely = width / (self.param['ny']-1)
         
         # y-coordinate (increasing latitude)
-        if hem > 0: # Northern Hemisphere
+        if self.param['hem'] > 0: # Northern Hemisphere
             self.y = np.linspace(0,width,self.param['ny'])
         else:
             self.y = np.linspace(-np.pi/2,width-np.pi/2,self.param['ny'])
         # z-coordinate [bottom,top)
-        self.z = np.arange(self.param['bottom']*self.param['Ho'],self.param['top']*self.param['Ho'],delz)
+        self.z = np.arange(self.param['bottom']*self.param['Ho'],self.param['top']*self.param['Ho'],self.delz)
         # time-step coordinate
         self.t = np.arange(self.param['nt'])
         
@@ -50,67 +51,75 @@ class grid:
         self.beta = 2*self.param['om']*np.cos(self.y) / self.param['ae']
         
         
-        def interp2z(self,data,pressure,n_filter=None):
-            '''
-                Interpolate data from pressure-coordinates to vertical grid with constant log-pressure spacing
+    def interp2z(self,data,pressure,nfilter=None):
+        '''
+            Interpolate data from pressure-coordinates to vertical grid with constant log-pressure spacing
                 
-                pressure in Pa, assume 
-            '''
-            ps = np.max(pressure)
-            zi = np.log(-pressure/ps)*self.param['Ho']
-            result = data.apply_along_axis(interpolation,zi,self.z)
-            if not(nfilter is None):
-                for n in range(n_filter):
-                    result = _filter(result,-2)
-            return result
+            pressure in Pa, assume 
+        '''
+        ps = np.max(pressure)
+        zi = (-1)*np.log(pressure/ps)*self.param['Ho']
+        result = np.apply_along_axis(_interpolation,-2,data,zi,self.z,)
+                                     #shape=(self.param['nz'],),dtype=data.dtype)
+        if not(nfilter is None):
+            for n in range(nfilter):
+                result = _filter(result,-2)
+        return result
                 
-            
-        def partial_y(self,data):
-            '''
-                First y-derivative
-            '''
-            derv = lambda a: (a[:,:,2:] - a[:,:,:-2]) / 2 / self.dely
-            result = data.map_overlap(derv,depth=1,boundary='reflect')
-            return result
+    def partial_y(self,data):
+        '''
+            First y-derivative
+        '''
+        derv = lambda a: (a[:,:,2:] - a[:,:,:-2]) / 2 / self.dely
+        #result = data.map_overlap(derv,depth=1,boundary='reflect',trim=False)
+        result = derv(np.pad(data,((0,0),(0,0),(1,1)),mode='edge'))
+        return result
 
-        def partial_yy(self,data):
-            '''
-                Second second y-derivative
-            '''
-            derv = lambda a: (a[:,:,2:] + a[:,:,:-2] - 2*a[:,:,1:-1]) / self.dely / self.dely
-            result = data.map_overlap(derv,depth=1,boundary='reflect')
+    def partial_yy(self,data):
+        '''
+            Second second y-derivative
+        '''
+        derv = lambda a: (a[:,:,2:] + a[:,:,:-2] - 2*a[:,:,1:-1]) / self.dely / self.dely
+        #result = data.map_overlap(derv,depth=1,boundary='reflect',trim=False)
+        result = derv(np.pad(data,((0,0),(0,0),(1,1)),mode='edge'))
+        return result
             
             
-        def partial_z(self,data):
-            '''
-                First z-derivative 
-            '''
-            derv = lambda a: (a[:,2:,:] - a[:,:-2,:]) / 2 / self.delz
-            result = data.map_overlap(derv,depth=1,boundary='nearest')
-            return result
+    def partial_z(self,data):
+        '''
+            First z-derivative 
+        '''
+        derv = lambda a: (a[:,2:,:] - a[:,:-2,:]) / 2 / self.delz
+        #result = data.map_overlap(derv,depth=1,boundary='nearest',trim=False)
+        result = derv(np.pad(data,((0,0),(1,1),(0,0)),mode='edge'))
+        return result
         
             
-        def partial_zz(self,data,N2):
-            '''
-                Second z-derivative taking into account density and N2 effects
-                (rho/N2*f_z)_z -> expand product rule in diff
-            '''
-            factor = np.exp(self.z/self.param['Ho']) / 2 / self.delz / self.delz
-            rho = np.exp(-self.z/self.param['Ho'])
-            roN2 = 1 / N2 * rho[:,np.newaxis]
-            
-            diff = lambda a,roN2: ((roN2[:,2:,:]+roN2[:,1:-1,:])*a[:,2:,:] - 
-                                   (roN2[:,2:,:]+2*roN2[:,1:-1,:]+roN2[:,:-2,:])*a[:,1:-1,:] + 
-                                   (roN2[:,1:-1,:]+roN2[:,:-2,:])*a[:,:-2,:])
-            result = data.map_overlap(diff,depth=1,boundary=nearest)
-            result = result * factor[:,np.newaxis]
-            return result
+    def partial_zz(self,data,N2):
+        '''
+            Second z-derivative taking into account density and N2 effects
+            (rho/N2*f_z)_z -> expand product rule in diff
+        '''
+        factor = np.exp(self.z/self.param['Ho']) / 2 / self.delz / self.delz
+        rho = np.exp(-self.z/self.param['Ho'])
+        roN2 = 1 / N2 * rho[:,np.newaxis]
+        
+        diff = lambda a,roN2: ((roN2[:,2:,:]+roN2[:,1:-1,:])*a[:,2:,:] - 
+                               (roN2[:,2:,:]+2*roN2[:,1:-1,:]+roN2[:,:-2,:])*a[:,1:-1,:] + 
+                               (roN2[:,1:-1,:]+roN2[:,:-2,:])*a[:,:-2,:])
+        #result = data.map_overlap(diff,depth=1,boundary=nearest,trim=False)
+        result = diff(np.pad(data,((0,0),(1,1),(0,0)),mode='edge'),np.pad(roN2,((0,0),(1,1),(0,0)),mode='edge'))
+        result = result * factor[:,np.newaxis]
+        return result
 
             
             
             
 def _interpolation(a,xi,x):
-    f = interp1d(xi,a,kind='cubic')
+    '''
+        Cubic spline interpolation
+    '''
+    f = interp1d(xi,a,kind='cubic',bounds_error=False,fill_value=np.nan)
     result = f(x)
     return result
     
@@ -125,7 +134,8 @@ def _filter(data,axis,kernel=np.array([0.25,0.5,0.25])):
     '''
         Smooth data along axis by convolution with kernel
     '''
-    result = data.apply_along_axis(convolve1d,axis,kernel,mode='mirror')
+    result = np.apply_along_axis(convolve1d,axis,data,kernel,mode='mirror',)
+                                 #shape=(data.shape[axis],),dtype=data.dtype)
     return result
     
     
@@ -136,14 +146,14 @@ def buoancy_frequency(T,grid):
     '''
         Calculate buoancy frequancy N2 from zonal mean temperature
     '''
-    N2 = (grid.partial_z(T)+T*2/7/grid.param['Ho'])*Ro/Ho
+    N2 = (grid.partial_z(T)+T*grid.param['kappa']/grid.param['Ho'])*grid.param['Ro']/grid.param['Ho']
     for n in range(10):
         N2 = _filter(N2,-2)
     return N2
     
     
     
-def PV_grad(U,N2,grid):
+def pv_gradient(U,N2,grid):
     '''
         Calculate the PV gradient from zonal-mean zonal wind
         taking into account N2 effects for the vertical derivative
@@ -153,8 +163,11 @@ def PV_grad(U,N2,grid):
     tany = np.tan(grid.y)
     cosy = np.cos(grid.y)
     
-    qy = U/cosy/cosy+grid.partial_y(U)*tany+grid.partial_yy(U)-grid.partial_zz(U)*grid.fcor*grid.fcor
-    qy = 1/grid.param['ae']**2 * qy + grid.beta
+    qy = U/cosy/cosy+grid.partial_y(U)*tany
+    # division by cosy produces out-of-bounds values
+    qy[:,:,0] = 0
+    qy[:,:,-1]  = 0
+    qy = 1/grid.param['ae']**2 * (qy-grid.partial_yy(U)) - grid.partial_zz(U,N2)*grid.fcor*grid.fcor + grid.beta
     return qy
     
     
